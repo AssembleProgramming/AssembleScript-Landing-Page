cb13e08d5029901963623be06e768e759662a0b5
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-07-20 17:08:23 +0530

    🧪 Fix Favicon test 3

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,21 @@ import './style.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Loader from './Pages/Loader/Loader.jsx';
 
+// Import the favicon.ico file
+import favicon from './assets/logo.png';
+// Function to set the favicon dynamically
+function setFavicon() {
+  const head = document.querySelector('head');
+  const link = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'icon';
+  link.href = favicon;
+  head.appendChild(link);
+}
+
+// Call the setFavicon function before rendering the app
+setFavicon();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Suspense fallback={<Loader />}>
     <App/>
