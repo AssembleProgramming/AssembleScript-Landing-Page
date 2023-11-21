f827e72f2bddfb8bc1c4175c8e67793da6a95c5f
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2023-11-21 20:55:40 +0530

    📆 Update start time

--- src/Pages/Contest/StartTime.js
+++ src/Pages/Contest/StartTime.js
@@ -1,3 +1,3 @@
 // (Note: Months are 0-based, so November is represented as 10)
-export const startTime = new Date(2023, 10, 3, 10, 30, 0); // 3rd DEC 2023, 10:30 AM 
+export const startTime = new Date(2023, 11, 3, 10, 30, 0); // 3rd DEC 2023, 10:30 AM 
 export const endTime = new Date(2023, 11, 3, 12, 0, 0); // 3rd November 2023, 12:00 AM
